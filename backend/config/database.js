6696f3435b26c670238e090b4a41aaa4687dd7cb
LaurentiuCotaga <34377018+LaurentiuCotaga@users.noreply.github.com>
2023-06-09 18:39:21 +0300

    connect postgresssql

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,25 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '0.0.0.0'),
+      port: env.int('DATABASE_PORT', 32768),
+      database: env('DATABASE_NAME', 'mirror-md'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '2313'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env.bool('DATABASE_SSL', false) && {
+        key: env('DATABASE_SSL_KEY', undefined),
+        cert: env('DATABASE_SSL_CERT', undefined),
+        ca: env('DATABASE_SSL_CA', undefined),
+        capath: env('DATABASE_SSL_CAPATH', undefined),
+        cipher: env('DATABASE_SSL_CIPHER', undefined),
+        rejectUnauthorized: env.bool(
+          'DATABASE_SSL_REJECT_UNAUTHORIZED',
+          true
+        ),
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
